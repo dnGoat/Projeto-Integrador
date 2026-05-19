5cd2531fd7601b849ec99ebf39dda7f3dfc3dca1
Aluno <aluno.formoso@ifto.local>
2026-05-19 17:04:42 -0300

    Subindo Arquivo Para GitHub

---
+++ POO/carro.py
@@ -0,0 +1,50 @@
+# A palavra "class" é usada para criar uma classe.
+# Uma classe funciona como um molde para criar objetos
+class Carro:
+
+    # "def" definir uma função ou método.
+    # "__init__" é o método construtor da classe.
+    # Ele é executado automaticamente quando um objeto é criado
+
+    # "self" representa o próprio objeto.
+    # É através do self que acessamos atributos e métodos do objeto
+
+    # "marca", "modelo", "ano" e "velocidade"
+    # São Parâmetros recebidos pela classe.
+
+
+    # Metodo construtor
+    def __init__(self, marca, modelo, ano, velocidade=0):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.velocidade = velocidade
+
+    # Metodos
+    # Metodo acelerar
+    def acelerar(self, aumento):
+        # self.velocidade = self.velocidade + aumento 
+        self.velocidade += aumento
+
+        print(f"O carro acelerou para{self.velocidade} km/h")
+
+
+# Criando um objeto da Classe Carro
+
+# "carro1" é uma variável que recebe um objeto
+carro1 = Carro("Chevrolet", "S10", 2013)
+
+# Exibir informações do carro
+print(f"Marca: {carro1.marca}")
+print(f"Modelo: {carro1.modelo}")
+print(f"Ano: {carro1.ano}")
+print(f"Velocidade: {carro1.velocidade}")
+
+carro1.acelerar(280)
+
+carro2 = Carro("Toyota", "Corolla", 2019)
+
+# Exibir informações do carro
+print(f"Marca: {carro2.marca}")
+print(f"Modelo: {carro2.modelo}")
+print(f"Ano: {carro2.ano}")
